b9a0ff5ef6ef9bdfbd430a560babe9385e72d4bd
Vinicius-Bitencourt-Pereira <viniciusbitencourtpereira@gmail.com>
2023-03-07 19:25:55 -0300

    findById resource, and exceptions

--- src/main/java/br/com/vinicius/clientapi/services/ClientService.java
+++ src/main/java/br/com/vinicius/clientapi/services/ClientService.java
@@ -1,5 +1,7 @@
 package br.com.vinicius.clientapi.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -9,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import br.com.vinicius.clientapi.dtos.ClientDTO;
 import br.com.vinicius.clientapi.entities.ClientEntity;
 import br.com.vinicius.clientapi.repositories.ClientRepository;
+import br.com.vinicius.clientapi.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
@@ -20,5 +23,11 @@ public class ClientService {
 	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
 		Page<ClientEntity> clients = clientRepository.findAll(pageRequest);
 		return clients.map(x -> new ClientDTO(x));
+	}
+
+	public ClientDTO findById(Long id) {
+		Optional<ClientEntity> result = clientRepository.findById(id);
+		ClientEntity entity = result.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		return  new ClientDTO(entity);
 	}
 }
