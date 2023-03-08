46381715f0d9b78a67f84d5a953cf015fc30c0bd
Vinicius-Bitencourt-Pereira <viniciusbitencourtpereira@gmail.com>
2023-03-07 21:03:34 -0300

    Delete resource

--- src/main/java/br/com/vinicius/clientapi/services/ClientService.java
+++ src/main/java/br/com/vinicius/clientapi/services/ClientService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -62,6 +63,16 @@ public class ClientService {
 		}
 		catch(EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
+	
+	@Transactional
+	public void delete(Long id) {
+		try {
+			clientRepository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("id not found" + id);
 		}
 	}
 }
